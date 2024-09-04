5ae3c5225e9ff910fcd9136854d899e249fb47ff
Tyrone Johnson <164740697+TyJhnsnD@users.noreply.github.com>
2024-09-03 19:41:32 -0600

    Update CSVWithHeaders.java

--- ProyectoUnidad1Arqui/src/main/java/verificaciones/CSVWithHeaders.java
+++ ProyectoUnidad1Arqui/src/main/java/verificaciones/CSVWithHeaders.java
@@ -4,10 +4,30 @@ import java.util.ArrayList;
 
 public class CSVWithHeaders extends BaseMiddleware<ArrayList<ArrayList<String>>> {
 
+    private ArrayList<String> header;
+    private ArrayList<ArrayList<String>> info
+
     @Override
     public boolean check(ArrayList<ArrayList<String>> info) {
+        this.info = info;
+        if(!getFirstLineElement(getFirstLineFile()).equals(getFirstLineElement(header)) 
+        && getSecondLineElement(getFirstLineFile()).equals(getSecondLineElement(header))){
+            return false;
+        }
         // TODO
         System.out.println("CSV_ConEncabezados");
         return checkNext(info);
+    }
+
+    private ArrayList<String> getFirstLineFile(){
+        return this.info.get(0);
+    }
+
+    private String getFirstLineElement(ArrayList<String> line){
+        return line.get(0);
+    }
+
+    private String getSecondLineElement(ArrayList<String> line){
+        return line.get(1);
     }
 }
