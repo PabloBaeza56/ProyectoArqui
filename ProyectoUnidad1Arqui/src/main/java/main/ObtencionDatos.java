ca27e6aa2bfdd50f3d3b77798f381c2575599354
Gonzalez Alvarez Maria Fernanda <99632505+mierri@users.noreply.github.com>
2024-09-03 23:40:51 -0600

    Método para obtener ruta absoluta del archivo
    El nuevo método permite seleccionar un archivo del explorador de archivos, para no ingresar la ruta manualmente.

--- ProyectoUnidad1Arqui/src/main/java/main/ObtencionDatos.java
+++ ProyectoUnidad1Arqui/src/main/java/main/ObtencionDatos.java
@@ -1,18 +1,19 @@
 package main;
 
 import java.util.ArrayList;
+import javax.swing.JFileChooser;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import lectores.ArchivoCSV;
 import lectores.ArchivoPDF;
 import lectores.CSVInvalidoException;
 import lectores.PDFInvalidoException;
 import lombok.Getter;
 
-
 public class ObtencionDatos {
     @Getter private ArrayList<ArrayList<String>> datosArchivoCSV;
     @Getter private ArrayList<String> datosArchivoPDF; 
-    
-    public void ejecutar(String rutaAbsolutaPDF, String rutaAbsolutaCSV){
+
+    public void ejecutar(String rutaAbsolutaPDF, String rutaAbsolutaCSV) {
         ArchivoPDF arc = new ArchivoPDF(rutaAbsolutaPDF);
         ArchivoCSV cv = new ArchivoCSV(rutaAbsolutaCSV);
         try {
@@ -26,4 +27,26 @@ public class ObtencionDatos {
             this.datosArchivoPDF = null;
         }
     } 
+    
+    public String seleccionarArchivo(String tipoArchivo) {
+        JFileChooser fileChooser = new JFileChooser();
+        FileNameExtensionFilter filter;
+
+        if (tipoArchivo.equals("PDF")) {
+            filter = new FileNameExtensionFilter("Archivos PDF", "pdf");
+        } else if (tipoArchivo.equals("CSV")) {
+            filter = new FileNameExtensionFilter("Archivos CSV", "csv");
+        } else {
+            filter = new FileNameExtensionFilter("Todos los Archivos", "*");
+        }
+
+        fileChooser.setFileFilter(filter);
+        int resultado = fileChooser.showOpenDialog(null);
+
+        if (resultado == JFileChooser.APPROVE_OPTION) {
+            return fileChooser.getSelectedFile().getAbsolutePath();
+        } else {
+            return null;
+        }
+    }
 }
